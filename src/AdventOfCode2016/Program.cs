b205db61013f18361ed227942efd482a6666dd98
Erik B <eribje@gmail.com>
2017-04-26 22:11:52 +0200

    Finished day 6.

--- src/AdventOfCode2016/Program.cs
+++ src/AdventOfCode2016/Program.cs
@@ -9,9 +9,9 @@ namespace AdventOfCode2016
         public delegate T DayRunner<T>(string input, out T output);
 
         static void Main(string[] args)
-        {
-            RunTest<string, int>("1", Inputs.InputDay1, Day1.SolvePart1);            
-            RunTest<string, string>("5b", Inputs.InputDay5, Day5.SolvePart2);
+        {            
+            RunTest<string, string>("6", Inputs.InputDay6, Day6.SolvePart1);
+            RunTest<string, string>("6b", Inputs.InputDay6, Day6.SolvePart2);
         }
 
         private static void RunTest<T,U>(string dayNumber, T input, Func<T, U> f) where T : class
